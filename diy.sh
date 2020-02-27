457300d89dfcb23cd40fbab94eae838a1d133c66
elonj <56976129+elonj@users.noreply.github.com>
2020-02-27 15:29:55 -0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,12 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+#2. Set default timezone to Los Angeles. Time zone info can be found in https://oldwiki.archive.openwrt.org/doc/uci/system
+#uci set system.@system[0].timezone=PST8PDT,M3.2.0,M11.1.0
+#uci set system.@system[0].zonename=America/Los Angeles
+sed -i 's/CST-8/PST8PDT,M3.2.0,M11.1.0/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/Asia/America/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/Shanghai/Los Angeles/g' package/lean/default-settings/files/zzz-default-settings
