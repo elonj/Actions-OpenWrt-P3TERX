65fa0224c457ed2c2df74c4367351e39c5fe4da6
elonj <56976129+elonj@users.noreply.github.com>
2020-03-02 10:19:19 -0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,6 +11,9 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # Modify timezone (not sure useful or not)
 sed -i 's/UTC/PST8PDT,M3.2.0,M11.1.0/g' package/base-files/files/bin/config_generate
 
+# Modify timezone (not sure useful or not)
+#sed -i 's/UTC/PST8PDT,M3.2.0,M11.1.0/g' package/base-files/files/etc/init.d/system
+
 #2. Set default timezone to Los Angeles. Time zone info can be found in https://oldwiki.archive.openwrt.org/doc/uci/system
 #uci set system.@system[0].timezone=PST8PDT,M3.2.0,M11.1.0
 #uci set system.@system[0].zonename=America/Los Angeles
